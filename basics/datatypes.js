2b4201334bc460d0566f9d705cc35b098eb94445
jaydeep <52592034+jaydeep-talaviya@users.noreply.github.com>
2024-06-17 15:15:02 +0000

    added file for data types

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treats all JS Code as newer version
+
+// alert(3+3) //we are using nodejs , not browser
+
+let name = "jay"
+let age = 24
+let isLoggedIn = false
+
+// number
+// bigint
+// string
+// boolean 
+// null -> standalon value
+// undefined -> 
+// symbol -> unique
+
+// object
+
+console.log( typeof "hello there")
+console.log( typeof 22)
+console.log( typeof null) //-> returns object because its one type of value it self
+
+console.log( typeof undefined) //-> always return undefined 
+
