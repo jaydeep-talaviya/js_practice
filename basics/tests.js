f0fb4ef3c7f9d11a5691d9dadf9bfdc9c21ed2f4
jaydeep <52592034+jaydeep-talaviya@users.noreply.github.com>
2024-06-17 14:33:50 +0000

    added practice for use or var and const

---
+++ basics/tests.js
@@ -0,0 +1,15 @@
+console.log("Jaydeep")
+
+const accountId = 144232
+var accountPassword = "12345"
+var accountEmail = "jaydeeptalaviya@gmail.com"
+accountCity = "Jaipur"
+
+
+accountEmail = "abc@gmail.com"
+accountPassword = "21231"
+accountCity = "Bengluru"
+
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
